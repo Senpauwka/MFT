e4febff5401ba7bab494f7f6bfcf02524d761552
Senpauwka <168306935+Senpauwka@users.noreply.github.com>
2026-08-16 06:04:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -926,15 +926,6 @@ function finishWorkout() {
 
     appData.stats.minutes += minutes;
 
-appData.history.push({
-    date: new Date().toISOString(),
-
-    workout: currentWorkout.title,
-
-    exercises: currentExerciseResults,
-
-    duration: minutes
-});
 
     saveData();
 
